bd05bb8626bcccd88adea3a482c98c41cf43090e
Nyseer Couse <nyseer.couse@gmail.com>
2024-10-21 16:56:26 -0400

    basic homepage layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
+import { Breadcrumbs,Grid2, Paper, Toolbar, Container, Button, Link, Typography, ButtonGroup, Box, ListItem } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //container for every component in website
+    <Container sx={{ color: 'black',
+      bgcolor: "white",
+       p: 1,
+       display: 'flex',
+       justifyContent: 'space-evenly',
+       flexDirection: {xs: 'column', md: 'column'}
+       }}>
+        {/* links container */}
+        <Container sx={{ color: 'black',
+          bgcolor: "white",
+          p: 1,
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          textAlign: 'center',
+          flexDirection: {xs: 'column', sm: 'row'},
+          fontSize: 10,
+          alignItems: 'center'
+          }}>
+          <img
+        srcSet={`${'logo.png'}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+        src={`${"logo.png"}?w=164&h=164&fit=crop&auto=format`}
+        alt={"logo_image"}
+        loading="lazy" 
+      />
+ 
+          {/* current page */}
+          <Link underline="hover" color="inherit" href="/">  
+            HOME
+          </Link>
+          <Link
+              underline="hover"
+              color="inherit"
+              href="/material-ui/getting-started/installation/"
+          >
+              ABOUT US
+          </Link>
+          <Link
+              underline="hover"
+              color="inherit"
+              href="/material-ui/getting-started/installation/"
+          >
+              WHAT WE TREAT
+          </Link>
+          <Link
+              underline="hover"
+              color="inherit"
+              href="/material-ui/getting-started/installation/"
+          >
+              OUR TEAM
+          </Link>
+          <Link
+              underline="hover"
+              color="inherit"
+              href="/material-ui/getting-started/installation/"
+          >
+              <Paper  sx={{ color: 'white',bgcolor: "#28b54d", p: 1}}>CONTACT US</Paper>
+          </Link>
+          
+        </Container>
+        {/* black bar */}
+        <Container>
+          <Container sx={{bgcolor: "#000000"}}>
+            <Typography sx={{color: "#28b54d", height: 20}}></Typography>
+          </Container>
+        </Container>
+        {/* image container */}
+        <Container sx={{ color: 'black',
+          bgcolor: "white",
+          p: 0,
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          textAlign: 'center',
+          flexDirection: {xs: 'column', sm: 'row'},
+          fontSize: 10,
+          alignItems: 'center',
+          height: 200,
+          }}>
+            <Container sx={{ color: 'black',
+          bgcolor: "blue",
+          width: 200,
+          height: 200,
+          }}>
+            image1
+            </Container>
+            <Container sx={{ color: 'black',
+          bgcolor: "pink",
+          width: 200,
+          height: 200,
+          }}>
+            image2
+            </Container>
+            <Container sx={{ color: 'black',
+          bgcolor: "red",
+          width: 200,
+          height: 200,
+          }}>
+            image3
+            </Container>
+
+
+          </Container>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, Typography } from '@mui/material';
 
+const theme = createTheme({
+  pallete:{
+    primary:{
+      main: "#28b54d",
+    },
+  },
+  button:{
+    color: "#28b54d"
+  }
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    
   </React.StrictMode>
 );
 
