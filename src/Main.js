d8e4fa5572c45ce843193e18eeac2a635a709784
nyseer1 <nyseer.couse@gmail.com>
2024-12-05 12:18:02 -0500

    cleaned up code

--- src/Main.js
+++ src/Main.js
@@ -104,67 +104,26 @@ function Main() {
             </Grid2>
           </Grid2>
           
+          <Grid2 size={6}>
+            <Grid2 item>
+            <Paper sx={{p:2}}>
+              <Typography sx={{fontSize:20,color:'#000000'}}>
+                -See us without a prescription
+                <br/>
+                -Individualized Physical Therapy experience
+                <br/>
+                -Accepts most Insurances
+                <br/>
+                -Appointment Available within 24 hours 
+              </Typography>
+          
+            </Paper>
 
-          <Grid2 size={3}>
-            <Grid2 item>{/* image */}
-              <Box component="img" sx={{width: '100%' , height: '100%', borderRadius:'2%',border:1}}
-                        alt={"Physical Therapy Example"}
-                        src={"Imperium_Physical_Therapy_and_Performance_Building.jpg"}/>
             </Grid2>
           </Grid2>
 
+
         </Grid2>
-
-      {/* content */}
-      
-      <Box
-        sx={{
-          p: 2,
-          display: "flex", //creates flexbox to arrange content
-          flexDirection: ["column", "column", "row", "row", "row"], //each new content is added in a row if screen > md, columb if <= md
-          justifyContent: "space-evenly", //content starts at the end of the line (right)
-          flexGrow: 1, //stretch size of elements to fill unused space on line
-          alignItems: "center", //centers items inside box horizontally (for when column)
-        }}
-      >
-        <Typography sx={{ fontSize: [20,18,20], textAlign: "center", p:1, color:'#000000' }}>
-          Expert Physical Therapy 
-          <Typography sx={{ fontSize: [25,32,50], textAlign: "center", p:1 }}>   
-            Giving You the Power to<br/>Improve Your Life
-          </Typography>
-          
-          <Paper
-            sx={{
-              fontSize: [16,16,30],
-              
-              p: 3,
-            }}
-          >
-            All of the specialized clinicians at Imperium Physical Therapy and Performance are dedicated to provide you with the highest quality of care. 
-          </Paper>
-          <Button href="/ourteam" sx={{display:'flex', flexDirection:'row',justifyContent:'center', p:3}}><Typography sx={{fontSize:[20,30,50]}}>learn more</Typography> <ArrowForwardIosIcon/> </Button>
-          <Divider sx={{p:1}}/>
-          <br/>
-         <Paper sx={{p:2}}>
-          <Typography sx={{fontSize:20,color:'#000000'}}>
-            -See us without a prescription
-            <br/>
-            -Individualized Physical Therapy experience
-            <br/>
-            -Accepts most Insurances
-            <br/>
-            -Appointment Available within 24 hours 
-          </Typography>
-          
-         </Paper>
-        </Typography>
-        
-        <Box component="img" sx={{width: '25%' , height: '25%', borderRadius:'2%'}}
-                  alt={"Physical Therapy Service"}
-                  src={"imperium_physical_therapy_services1.JPG"}/>
-        
-      </Box>
-
 
       
       <br/><br/><br/><br/><br/><br/><br/><br/><br/>
