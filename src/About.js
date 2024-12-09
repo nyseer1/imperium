6ce89862519a2b04937f70aa65db0c310651e8f2
mario814 <m.guerrero814@gmail.com>
2024-12-08 21:18:42 -0500

    Update About.js
    Just removed a bit of extra code

--- src/About.js
+++ src/About.js
@@ -77,16 +77,16 @@ function About() {
             gutterBottom
             sx={{ font: [2], color: "#000000" }}
           >
-            We are a <>physical therapy</> practice that specializes in the diagnoses
+            We are a physical therapy practice that specializes in the diagnoses
             and rehabilitation of orthopedic related conditions, management of
-            <> post operative cases</> and overall optimization of functional ability
-            including <>restoration</> of mobility, range of motion and strength.
+             post operative cases and overall optimization of functional ability
+            including restoration of mobility, range of motion and strength.
           </Typography>
           <Typography
             variant="body1"
             gutterBottom
             sx={{ font: [2], color: "#000000" }}
-          > 
+          >
             Our mission is to improve the health and wellness of our clients by
             empowering them with the knowledge and skills to optimize their
             physical potential. Our vision is to develop championship athletes
